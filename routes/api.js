04e8d3ac314355497771f3d18f49fe6c8b2123fd
ghost-fk <fayyad_r22@yahoo.com>
2018-10-02 22:53:58 +0200

    get the json from the data and exports and use in server js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,14 @@
 const path = require('path');
 const fs = require('fs')
 const express = require('express');
-const {promisify} = require('util');
-const readFile = promisify(readFile);
-const writeFile = promisify(writeFile);
+
 const router = express.Router();
+
+
+
+router.get('/students',(req, res) =>{
+  res.sendFile(path.join(__dirname, '..', 'data', 'students.json'))
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const path = require('path')
 const args = process.argv.slice(2);
 const port = args[0] || 3000
 const express = require('express');
-
+const apiRouter = require('./routes/api');
 const app= express();
 
 app.use(express.static('static/student-roster'))
 
-app.get('/students', (req, res) =>{
-  res.sendFile(path.join(__dirname, 'data', 'students.json'))
-});
+
+app.use('/api', apiRouter)
 
 app.listen(port, () =>{
   console.log(`you are listen in port ${port}`)
