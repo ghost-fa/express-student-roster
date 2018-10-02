571535efc32fcd82339978773e9821b85abf0e5f
ghost-fk <fayyad_r22@yahoo.com>
2018-10-02 23:07:25 +0200

    add POST method route and and try post in postman

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const fs = require('fs')
 const express = require('express');
-
+const {promisify} = require('util');
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 const router = express.Router();
 
 
@@ -10,5 +12,8 @@ router.get('/students',(req, res) =>{
   res.sendFile(path.join(__dirname, '..', 'data', 'students.json'))
 });
 
+router.post('/students', express.urlencoded(), (req, res) =>{
+  res.send('your rquest is send')
+})
 
 module.exports = router;
